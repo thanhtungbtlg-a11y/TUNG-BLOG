d4e1601fa3bc7ddab46eeeee62e76320b013ad6a
thanhtungbtlg-a11y <thanhtungbtlg@gmail.com>
2026-06-20 18:52:44 +0700

    Update profile visuals and add blog guide

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "assets/images/site-banner.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
@@ -49,7 +49,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/wp1892096.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/profile-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Nguyễn Thanh Tùng",
 	bio: "Just a boy caught up in dreams and fantasies",
 	links: [
